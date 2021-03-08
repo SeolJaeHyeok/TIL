6a31060808915f03e3693171f9df2bd1fefbc4bd
seoljaehyeok <milkboy2564@naver.com>
2021-03-08 21:52:59 +0900

    4장 이벤트 핸들링 내용정리

---
+++ hello-react/src/EventPractice.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요."
+          onChange={(e) => console.log(e.target.value)}
+        />
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
