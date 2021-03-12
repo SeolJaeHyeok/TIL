f33d44d5a6ca27a3b6ac5bbfa5d7b64b046fcb2c
seoljaehyeok <milkboy2564@naver.com>
2021-03-12 16:16:12 +0900

    4장 이벤트 핸들링 내용정리

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,19 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class EventPractice extends Component {
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력하세요."
-          onChange={(e) => console.log(e.target.value)}
-        />
-      </div>
-    );
-  }
-}
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const { username, message } = form;
+
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+      [e.target.name]: e.target.value, // 원하는 값을 덮어 씌우기
+    };
+    setForm(nextForm);
+  };
+  const handleClick = () => {
+    alert(username + ":" + message);
+    setForm({
+      username: "",
+      message: "",
+    });
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력하세요."
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={handleClick}>확인</button>
+    </div>
+  );
+};
 
 export default EventPractice;
