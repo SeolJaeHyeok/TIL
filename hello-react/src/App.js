0c0a4b6a2162213257d2d4988934bc2f0709c977
seoljaehyeok <milkboy2564@naver.com>
2021-03-14 02:32:37 +0900

    7장 라이프사이클 메서드  내용정리

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
+import ErrorBoundary from "./ErrorBoundary";
+import LifeCycleSample from "./LifeCycleSample";
+
+// 랜덤 색상을 생성
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
