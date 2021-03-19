493805c3f5222694cfe53050beea76ccb60947ee
seoljaehyeok <milkboy2564@naver.com>
2021-03-19 13:24:06 +0900

    9장 컴포넌트 스타일링 내용정리

---
+++ styling-react/src/App.js
@@ -0,0 +1,14 @@
+import SassComponent from "./SassComponent";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
