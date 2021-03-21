136dce2a635554a5f0be2d4091d384fffa306fb9
seoljaehyeok <milkboy2564@naver.com>
2021-03-21 23:07:43 +0900

    11장 컴포넌트 성능 최적화 내용정리

--- todo-app/src/components/ToDoList.js
+++ todo-app/src/components/ToDoList.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { List } from "react-virtualized";
 import ToDoListItem from "./ToDoListItem";
 import "./ToDoList.scss";
 
 const ToDoLIst = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="ToDoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <ToDoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [todos, onRemove, onToggle]
+  );
+
+  return (
+    <List
+      className="ToDoList"
+      width={512} // 전체 크기
+      height={513} // 전체 높이
+      rowCount={todos.length} // 항목 개수
+      rowHeight={57} // 항목 높이
+      rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수
+      list={todos} // 배열
+      style={{ outline: "none" }} // 리스트에 기본 적용되는 outline 스타일 제거
+    />
   );
 };
 
-export default ToDoLIst;
+export default React.memo(ToDoLIst);
--- todo-app/src/components/ToDoListItem.js
+++ todo-app/src/components/ToDoListItem.js
@@ -7,20 +7,25 @@ import {
 } from "react-icons/md";
 import "./ToDoListItem.scss";
 
-const ToDoListItem = ({ todo, onRemove, onToggle }) => {
+const ToDoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="ToDoListItem">
-      <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="ToDoListItem-virtualized" style={style}>
+      <div className="ToDoListItem">
+        <div
+          className={cn("checkbox", { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default ToDoListItem;
+export default React.memo(ToDoListItem);
