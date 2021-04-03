e149a6af6203b0fb7af2b407f87e08594bc05ae8
seoljaehyeok <milkboy2564@naver.com>
2021-04-03 21:19:29 +0900

    17장 리덕스 상태 관리 - TodosContainer 작성

--- Art_of_React/react-redux-tutorial/src/modules/todos.js
+++ Art_of_React/react-redux-tutorial/src/modules/todos.js
@@ -63,8 +63,8 @@ function todos(state = initialState, action) {
     case TOGGLE:
       return {
         ...state,
-        totos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todos, done: !todo.done } : todo
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo
         ),
       };
     case REMOVE:
