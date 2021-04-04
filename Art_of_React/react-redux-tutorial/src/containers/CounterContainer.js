a8e85a65329ccfeb668ec15cf1918f9f7c06f09c
seoljaehyeok <milkboy2564@naver.com>
2021-04-05 03:26:42 +0900

    17장 리덕스 상태 관리 - Done

--- Art_of_React/react-redux-tutorial/src/containers/CounterContainer.js
+++ Art_of_React/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,20 +1,16 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { decrease, increase } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  }
-)(CounterContainer);
+export default CounterContainer;
