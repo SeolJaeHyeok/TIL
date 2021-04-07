b80475a74aeddbcd90325c05a663460727e7ec6a
seoljaehyeok <milkboy2564@naver.com>
2021-04-07 18:45:16 +0900

    18장 리덕스 미들웨어 - 미들웨어란?

--- Art_of_React/learn-redux-middleware/src/index.js
+++ Art_of_React/learn-redux-middleware/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
