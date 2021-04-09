394cb536f054180ad89237ed210a07e8546e5135
seoljaehyeok <milkboy2564@naver.com>
2021-04-09 19:31:33 +0900

    18장 리덕스 미들웨어 - redux-thunk 마무리

--- Art_of_React/learn-redux-middleware/src/containers/SampleContainer.js
+++ Art_of_React/learn-redux-middleware/src/containers/SampleContainer.js
@@ -13,8 +13,14 @@ const SampleContainer = ({
   loadingUsers,
 }) => {
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+    // 그 내부에서 async 함수를 선언하고 호출해 준다.
+    try {
+      getPost(1);
+      getUsers(1);
+    } catch (e) {
+      console.log(e); // 에러 조회
+    }
   }, [getPost, getUsers]);
   return (
     <Sample
@@ -27,11 +33,11 @@ const SampleContainer = ({
 };
 
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS,
+    loadingPost: loading["sample/GET_POST"],
+    loadingUsers: loading["sample/GET_USERS"],
   }),
   {
     getPost,
