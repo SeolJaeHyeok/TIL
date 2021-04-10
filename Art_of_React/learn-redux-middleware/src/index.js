973ba62260a1129ef3ad0bb416f2ef8a3bf671b3
seoljaehyeok <milkboy2564@naver.com>
2021-04-10 22:28:27 +0900

    18장 리덕스 미들웨어 - redux-saga #1

--- Art_of_React/learn-redux-middleware/src/index.js
+++ Art_of_React/learn-redux-middleware/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 import { createLogger } from "redux-logger";
 import ReduxThunk from "redux-thunk";
+import creaseSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = creaseSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
--- Art_of_React/learn-redux-middleware/src/modules/index.js
+++ Art_of_React/learn-redux-middleware/src/modules/index.js
@@ -1,12 +1,18 @@
 import { combineReducers } from "redux";
-import counter from "./couter";
+import counter, { counterSaga } from "./couter";
 import sample from "./sample";
 import loading from "./loading";
+import { all } from "redux-saga/effects";
 
 const rootReducer = combineReducers({
   counter,
   sample,
   loading,
 });
+
+export function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐 주는 역할을 한다.
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
