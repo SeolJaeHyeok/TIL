42f365f841fc999f97c0784a344099565a189653
seoljaehyeok <milkboy2564@naver.com>
2021-04-16 17:18:21 +0900

    서버 사이드 렌더링 - redux-thunk 코드 작성

--- Art_of_React/ssr-recipe/src/index.js
+++ Art_of_React/ssr-recipe/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
 import App from "./App";
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ Art_of_React/ssr-recipe/src/modules/users.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+// 액션 타입 정의
+const GET_USERS_PENDING = "users/GET_USERS_PENDING";
+const GET_USERS_SUCCESS = "users/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "users/GET_USERS_FAILURE";
+
+// 액션 생성 함수
+const getUsersPending = () => ({ type: GET_USERS_PENDING });
+const getUsersSuccess = (payload) => ({ type: GET_USERS_SUCCESS, payload });
+const getUsersFailure = (payload) => ({
+  type: GET_USERS_FAILURE,
+  error: true,
+  payload,
+});
+
+// redux-thunk 함수
+export const getUsers = () => async (dispatch) => {
+  try {
+    dispatch(getUsersPending());
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    dispatch(getUsersSuccess(response));
+  } catch (e) {
+    dispatch(getUsersFailure(e));
+    throw e;
+  }
+};
+
+// 초기 상태 지정
+const initialState = {
+  users: null,
+  user: null,
+  loading: {
+    users: false,
+    users: false,
+  },
+  error: {
+    users: null,
+    user: null,
+  },
+};
+
+// 리듀서 함수
+function users(state = initialState, action) {
+  switch (action.type) {
+    case GET_USERS_PENDING:
+      return { ...state, loading: { ...state.loading, users: true } };
+    case GET_USERS_SUCCESS:
+      return {
+        ...state,
+        loading: { ...state.loading, users: false },
+        users: action.payload.data,
+      };
+    case GET_USERS_FAILURE:
+      return {
+        ...state,
+        loading: { ...state.loading, users: false },
+        error: { ...state.error, users: action.payload },
+      };
+    default:
+      return state;
+  }
+}
+
+export default users;
