014216b1fd976ff399d88176e716ca1f4165844e
seoljaehyeok <milkboy2564@naver.com>
2021-04-21 00:31:47 +0900

    20장 SSR - usePreloader Hook 만들어 적용

--- Art_of_React/ssr-recipe/src/containers/UserContainer.js
+++ Art_of_React/ssr-recipe/src/containers/UserContainer.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import User from "../components/User";
-import { Preloader } from "../lib/PreloadContext";
+import { Preloader, usePreloader } from "../lib/PreloadContext";
 import { getUser } from "../modules/users";
 
 const UserContainer = ({ id }) => {
   const user = useSelector((state) => state.users.user);
   const dispatch = useDispatch();
+
+  // usePreloader Hook을 이용해서 SSR 하기 전 데이터를 API 요청
+  usePreloader(() => dispatch(getUser(id)));
 
   useEffect(() => {
     if (user && user.id === parseInt(id, 10)) return; // 사용자가 존재하고, id가 일치한다면 요청 x
@@ -14,11 +17,8 @@ const UserContainer = ({ id }) => {
   }, [dispatch, id, user]); // id가 바뀔 때 새로 요청해야 함
 
   // 컨테이너 유효성 검사 후 return null을 해야 하는 경우에
-  // null 대신 Preloader 반환
-  if (!user) {
-    return <Preloader resolve={() => dispatch(getUser(id))} />;
-  }
 
+  if (!user) return null;
   return <User user={user} />;
 };
 
