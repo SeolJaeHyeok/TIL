4916bc1c9de92af5c50d86f1fa64e23239d86e04
seoljaehyeok <milkboy2564@naver.com>
2021-04-20 21:31:26 +0900

    20장 SSR - redux-saga 코드 작성

--- Art_of_React/ssr-recipe/src/index.js
+++ Art_of_React/ssr-recipe/src/index.js
@@ -4,14 +4,19 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { applyMiddleware, createStore } from "redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   window.__PRELOADED_STATE__, // 이 값을 초기상태로 사용함
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
--- Art_of_React/ssr-recipe/src/modules/users.js
+++ Art_of_React/ssr-recipe/src/modules/users.js
@@ -1,8 +1,13 @@
 import axios from "axios";
+import { call, put, takeEvery } from "redux-saga/effects";
 
 const GET_USERS_PENDING = "users/GET_USERS_PENDING";
 const GET_USERS_SUCCESS = "users/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "users/GET_USERS_FAILURE";
+
+const GET_USER = "user/GET_USER";
+const GET_USER_SUCCESS = "user/GET_USER_SUCCESS";
+const GET_USER_FAILURE = "user/GET_USER_FAILURE";
 
 const getUsersPending = () => ({ type: GET_USERS_PENDING });
 const getUsersSuccess = (payload) => ({ type: GET_USERS_SUCCESS, payload });
@@ -10,6 +15,14 @@ const getUsersFailure = (payload) => ({
   type: GET_USERS_FAILURE,
   error: true,
   payload,
+});
+
+export const getUser = (id) => ({ type: GET_USER, payload: id });
+const getUserSuccess = (payload) => ({ type: GET_USER_SUCCESS, payload });
+const getUserFailure = (error) => ({
+  type: GET_USER_FAILURE,
+  payload: error,
+  error: true,
 });
 
 export const getUsers = () => async (dispatch) => {
@@ -25,6 +38,22 @@ export const getUsers = () => async (dispatch) => {
   }
 };
 
+const getUserById = (id) =>
+  axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+function* getUserSaga(action) {
+  try {
+    const response = yield call(getUserById, action.payload);
+    yield put(getUserSuccess(response.data));
+  } catch (e) {
+    yield put(getUserFailure(e));
+  }
+}
+
+export function* usersSaga() {
+  yield takeEvery(GET_USER, getUserSaga);
+}
+
 const initialState = {
   users: null,
   loading: {
@@ -39,23 +68,23 @@ const initialState = {
 
 function users(state = initialState, action) {
   switch (action.type) {
-    case GET_USERS_PENDING:
+    case GET_USER:
       return {
         ...state,
-        loading: { ...state.loading, users: true },
-        error: { ...state.error, users: null },
+        loading: { ...state.loading, user: true },
+        error: { ...state.error, user: null },
       };
-    case GET_USERS_SUCCESS:
+    case GET_USER_SUCCESS:
       return {
         ...state,
-        loading: { ...state.loading, users: false },
-        users: action.payload.data,
+        loading: { ...state.loading, user: false },
+        users: action.payload,
       };
-    case GET_USERS_FAILURE:
+    case GET_USER_FAILURE:
       return {
         ...state,
-        loading: { ...state.loading, users: false },
-        error: { ...state.error, users: action.payload },
+        loading: { ...state.loading, user: false },
+        error: { ...state.error, user: action.payload },
       };
     default:
       return state;
