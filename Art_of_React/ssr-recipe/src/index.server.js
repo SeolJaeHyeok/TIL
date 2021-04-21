4996083b6e738540891aa0927e289a8cb95eb5f8
seoljaehyeok <milkboy2564@naver.com>
2021-04-21 16:40:31 +0900

    20장 SSR - loadableReady와 hydrate 적용

--- Art_of_React/ssr-recipe/src/index.server.js
+++ Art_of_React/ssr-recipe/src/index.server.js
@@ -4,43 +4,43 @@ import express from "express";
 import { StaticRouter } from "react-router-dom";
 import App from "./App";
 import path from "path";
-import fs from "fs";
 import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware, { END } from "redux-saga";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import rootReducer, { rootSaga } from "./modules";
 import PreloadContext from "./lib/PreloadContext";
+import { END } from "redux-saga";
 import { ChunkExtractor, ChunkExtractorManager } from "@loadable/server";
 
+// asset-manifest.json 에서 파일 경로들을 조회합니다.
 const statsFile = path.resolve("./build/loadable-stats.json");
 
-function createPage(root, stateScript) {
+function createPage(root, tags) {
   return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1,shrink-to-fit=no"
-      />
-      <meta name="theme-color" content="#000000" />
-      <title>React App</title>
-      ${tags.styles}
-      ${tags.links}
-    </head>
-    <body>
-      <noscript>You need to enable JavaScript to run this app.</noscript>
-      <div id="root">
-        ${root}
-      </div>
-      ${tags.scripts}
-    </body>
-    </html>
-      `;
+  <html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,shrink-to-fit=no"
+    />
+    <meta name="theme-color" content="#000000" />
+    <title>React App</title>
+    ${tags.styles}
+    ${tags.links}
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root">
+      ${root}
+    </div>
+    ${tags.scripts}
+  </body>
+  </html>
+    `;
 }
-
 const app = express();
 
 // 서버사이드 렌더링을 처리 할 핸들러 함수입니다.
@@ -62,7 +62,7 @@ const serverRender = async (req, res, next) => {
     promises: [],
   };
 
-  // 필요한 파일을 추출하기 위한 ChunkExtractor
+  // 필요한 파일 추출하기 위한 ChunkExtractor
   const extractor = new ChunkExtractor({ statsFile });
 
   const jsx = (
@@ -77,28 +77,29 @@ const serverRender = async (req, res, next) => {
     </ChunkExtractorManager>
   );
 
-  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링합니다.
-  store.dispatch(END); // redux-saga의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료된다.
+  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup 으로 한번 렌더링합니다.
+  store.dispatch(END); // redux-saga 의 END 액션을 발생시키면 액션을 모니터링하는 saga 들이 모두 종료됩니다.
   try {
-    await sagaPromise; // 기존에 진행 중이던 사가들이 모두 끝날 때까지 기다린다.
+    await sagaPromise; // 기존에 진행중이던 saga 들이 모두 끝날때까지 기다립니다.
     await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
   } catch (e) {
-    return res.staus(500);
+    return res.status(500);
   }
   preloadContext.done = true;
-  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-
+  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 합니다.
   // JSON 을 문자열로 변환하고 악성스크립트가 실행되는것을 방지하기 위해서 < 를 치환처리
   // https://redux.js.org/recipes/server-rendering#security-considerations
   const stateString = JSON.stringify(store.getState()).replace(/</g, "\\u003c");
   const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`; // 리덕스 초기 상태를 스크립트로 주입합니다.
 
+  // 미리 불러와야 하는 스타일 / 스크립트를 추출하고
   const tags = {
     scripts: stateScript + extractor.getScriptTags(), // 스크립트 앞부분에 리덕스 상태 넣기
     links: extractor.getLinkTags(),
     styles: extractor.getStyleTags(),
   };
-  res.send(createPage(root, tags)); // 클라이언트에게 결과물을 응답합니다.
+
+  res.send(createPage(root, tags)); // 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve("./build"), {
@@ -108,7 +109,7 @@ const serve = express.static(path.resolve("./build"), {
 app.use(serve); // 순서가 중요합니다. serverRender 전에 위치해야 합니다.
 app.use(serverRender);
 
-// 5000포트로 서버를 가동합니다.
+// 5000 포트로 서버를 가동합니다.
 app.listen(5000, () => {
   console.log("Running on http://localhost:5000");
 });
