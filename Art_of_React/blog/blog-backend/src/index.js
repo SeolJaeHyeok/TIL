08515799a8706f43abd84b90af8f42b4f20cb0fb
seoljaehyeok <milkboy2564@naver.com>
2021-04-22 16:30:25 +0900

    20장 백엔드 프로그래밍 - Koa 기본 사용법

--- Art_of_React/blog/blog-backend/src/index.js
+++ Art_of_React/blog/blog-backend/src/index.js
@@ -2,6 +2,22 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  console.log(ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  await next();
+  console.log('END');
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
 app.use((ctx) => {
   ctx.body = 'hello world';
 });
