80c7d22b4b483ab80eed0b999e6c88b62f42d57e
seoljaehyeok <milkboy2564@naver.com>
2021-04-23 10:24:36 +0900

    21장 백엔드 프로그래밍 - koa-router 기본 사용법 및 라우트 파라미터 및 쿼리

--- Art_of_React/blog/blog-backend/src/index.js
+++ Art_of_React/blog/blog-backend/src/index.js
@@ -1,6 +1,30 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = 'Home';
+});
+
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 ${id}` : '포스트 아이디가 없습니다.';
+});
+router.get('/about', (ctx) => {
+  ctx.body = 'About';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
