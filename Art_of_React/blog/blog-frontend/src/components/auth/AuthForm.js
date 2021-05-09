34a6365ddf8c263ac85dae6a6cc2e50a419d461c
Jaehyeok <milkboy2564@naver.com>
2021-05-09 23:52:47 +0900

    24장 프런트엔드 프로젝트 - 회원가입 및 로그인 UI 틀 준비와 Snippet 사용 설정

---
+++ Art_of_React/blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * 회원 가입 또는 로그인 폼을 보여 준다.
+ */
+
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+  return <AuthFormBlock>AuthForm</AuthFormBlock>;
+};
+
+export default AuthForm;
