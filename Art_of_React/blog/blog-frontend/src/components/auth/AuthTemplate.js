c973a3a8a8f30004b1950e351211c81e2c179b9a
Jaehyeok <milkboy2564@naver.com>
2021-05-10 23:52:47 +0900

    24장 프런트엔드 프로젝트 - 회원 인증에 필요한 UI 완성

--- Art_of_React/blog/blog-frontend/src/components/auth/AuthTemplate.js
+++ Art_of_React/blog/blog-frontend/src/components/auth/AuthTemplate.js
@@ -1,14 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import { Link } from 'react-router-dom';
 
 /**
  * 회원가입/로그인 페이지의 레이아웃을 담당하는 컴포넌트
  */
 
-const AuthTemplateBlock = styled.div``;
+/* 화면 전체를 채움 */
+const AuthTemplateBlock = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: ${palette.gray[2]};
+  /* flex로 내부 내용 중앙 정렬*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+/* 흰색 박스 */
+const WhiteBox = styled.div`
+  .logo-area {
+    display: block;
+    padding-bottom: 2rem;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 2px;
+
+    .prj-name {
+      color: black;
+      text-decoration: none;
+    }
+  }
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.025);
+  padding: 2rem;
+  width: 360px;
+  background: white;
+  border-radius: 2px;
+`;
 
 const AuthTemplate = ({ children }) => {
-  return <AuthTemplateBlock>{children}</AuthTemplateBlock>;
+  return (
+    <AuthTemplateBlock>
+      <WhiteBox>
+        <div className="logo-area">
+          <Link to="/" className="prj-name">
+            REACTERS
+          </Link>
+        </div>
+        {children}
+      </WhiteBox>
+    </AuthTemplateBlock>
+  );
 };
 
 export default AuthTemplate;
--- Art_of_React/blog/blog-frontend/src/pages/RegisterPage.js
+++ Art_of_React/blog/blog-frontend/src/pages/RegisterPage.js
@@ -5,7 +5,7 @@ import AuthTemplate from '../components/auth/AuthTemplate';
 const RegisterPage = () => {
   return (
     <AuthTemplate>
-      <AuthForm />
+      <AuthForm type="register" />
     </AuthTemplate>
   );
 };
