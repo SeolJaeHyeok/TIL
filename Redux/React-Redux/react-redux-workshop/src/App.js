e81b953d7a4de7d0e5bdcca30553d0bc99fa0cf2
Jaehyeok <milkboy2564@naver.com>
2021-05-30 00:22:01 +0900

    Componentization of UI

--- Redux/React-Redux/react-redux-workshop/src/App.js
+++ Redux/React-Redux/react-redux-workshop/src/App.js
@@ -1,29 +1,14 @@
 import "./App.css";
+import Article from "./components/article";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>WEB</h1>
-        World Wide WEB.
-      </header>
-      <nav>
-        <ol>
-          <li>
-            <a href="html.html">HTML</a>
-          </li>
-          <li>
-            <a href="css.html">CSS</a>
-          </li>
-          <li>
-            <a href="javascript.html">JavaScript</a>
-          </li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB.
-      </article>
+      <Header />
+      <Nav />
+      <Article />
     </div>
   );
 }
---
+++ Redux/React-Redux/react-redux-workshop/src/components/Nav.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ol>
+          <li>
+            <a href="html.html">HTML</a>
+          </li>
+          <li>
+            <a href="css.html">CSS</a>
+          </li>
+          <li>
+            <a href="javascript.html">JavaScript</a>
+          </li>
+        </ol>
+      </nav>
+    );
+  }
+}
