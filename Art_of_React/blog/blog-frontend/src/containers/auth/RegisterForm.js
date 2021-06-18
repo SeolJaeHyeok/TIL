d1dd6376c5776c2c23e4e055fcb2385ad551b1aa
Jaehyeok <milkboy2564@naver.com>
2021-06-18 14:01:51 +0900

    Authentication #1 - Before Error fix

--- Art_of_React/blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ Art_of_React/blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
   }));
   // 인풋 변경 핸들러
   const onChange = (e) => {
@@ -23,7 +25,12 @@ const RegisterForm = () => {
   // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    // TODO
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      // TODO: 오류처리
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   // 컴포넌트가 처음 렌더링될 때 form을 초기화
@@ -31,6 +38,18 @@ const RegisterForm = () => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
 
+  // 회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+    }
+  }, [auth, authError]);
   return (
     <AuthForm
       type="register"
--- Art_of_React/blog/blog-frontend/src/modules/auth.js
+++ Art_of_React/blog/blog-frontend/src/modules/auth.js
@@ -1,42 +1,41 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
 import { takeLatest } from 'redux-saga/effects';
-import createRequestSaga from '../lib/createRequestSaga';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
 import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const REGISTER = 'auth/REGISTER';
-const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
-const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+  'auth/REGISTER',
+);
 
-const LOGIN = 'auth/LOGIN';
-const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
-const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  'auth/LOGIN',
+);
 
 export const changeField = createAction(
   CHANGE_FIELD,
   ({ form, key, value }) => ({
-    form, // register, login
+    form, // register , login
     key, // username, password, passwordConfirm
     value, // 실제 바꾸려는 값
   }),
 );
-
-export const initializeForm = createAction(INITIALIZE_FORM, (form) => form); // register, login
-
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form); // register / login
 export const register = createAction(REGISTER, ({ username, password }) => ({
   username,
   password,
 }));
-
 export const login = createAction(LOGIN, ({ username, password }) => ({
   username,
   password,
 }));
 
-// 사가 생성
+// saga 생성
 const registerSaga = createRequestSaga(REGISTER, authAPI.register);
 const loginSaga = createRequestSaga(LOGIN, authAPI.login);
 export function* authSaga() {
@@ -62,20 +61,20 @@ const auth = handleActions(
   {
     [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
       produce(state, (draft) => {
-        draft[form][key] = value; // 예: state.register.username을 바꾼다.
+        draft[form][key] = value; // 예: state.register.username을 바꾼다
       }),
     [INITIALIZE_FORM]: (state, { payload: form }) => ({
       ...state,
       [form]: initialState[form],
       authError: null, // 폼 전환 시 회원 인증 에러 초기화
     }),
-    // 회원 가입 성공
+    // 회원가입 성공
     [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
       ...state,
       authError: null,
       auth,
     }),
-    // 회원 가입 실패
+    // 회원가입 실패
     [REGISTER_FAILURE]: (state, { payload: error }) => ({
       ...state,
       authError: error,
