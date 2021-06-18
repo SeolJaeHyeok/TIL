8d21152e0f4bb0227397d045995738511f1660aa
Jaehyeok <milkboy2564@naver.com>
2021-06-18 16:23:50 +0900

    회원가입 기능 - 유저 상태 체크할 때 Unauthorized 해결 필요

--- Art_of_React/blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ Art_of_React/blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -2,15 +2,18 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
     authError: auth.authError,
+    user: user.user,
   }));
-  // 인풋 변경 핸들러
+  // 인풋 변경 이벤트 핸들러
   const onChange = (e) => {
     const { value, name } = e.target;
     dispatch(
@@ -27,18 +30,18 @@ const RegisterForm = () => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
     if (password !== passwordConfirm) {
-      // TODO: 오류처리
+      // TODO: 오류 처리
       return;
     }
     dispatch(register({ username, password }));
   };
 
-  // 컴포넌트가 처음 렌더링될 때 form을 초기화
+  // 컴포넌트가 처음 렌더링 될 때 form 을 초기화함
   useEffect(() => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
 
-  // 회원가입 성공/실패 처리
+  // 회원가입 성공 / 실패 처리
   useEffect(() => {
     if (authError) {
       console.log('오류 발생');
@@ -48,8 +51,19 @@ const RegisterForm = () => {
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
+      dispatch(check());
     }
-  }, [auth, authError]);
+  }, [auth, authError, dispatch]);
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      console.log('check API 성공');
+      console.log(user);
+      history.push('/'); // 홈 화면으로 이동
+    }
+  }, [history, user]);
+
   return (
     <AuthForm
       type="register"
@@ -60,4 +74,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
--- Art_of_React/blog/blog-frontend/src/lib/api/auth.js
+++ Art_of_React/blog/blog-frontend/src/lib/api/auth.js
@@ -9,4 +9,4 @@ export const register = ({ username, password }) =>
   client.post('/api/auth/register', { username, password });
 
 // 로그인 상태 확인
-export const checkLoginStatus = () => client.get('/api/auth/check');
+export const check = () => client.get('/api/auth/check');
