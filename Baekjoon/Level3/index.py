216af32ebfa97f820ea7c5acd95ed5dee69f6043
Jaehyeok <milkboy2564@naver.com>
2021-06-21 17:20:32 +0900

    Baekjoon - Level 3

---
+++ Baekjoon/Level3/index.py
@@ -0,0 +1,39 @@
+# 2739번
+# num = int(input())
+# for i in range(1,10):
+#     print('{} * {} = {}'.format(num, i, num*i))
+
+# 10950번
+# T = int(input())
+# result = []
+# for i in range(T):
+#     num1, num2 = map(int, input().split())
+#     result.append(num1+num2)
+#
+# for i in result:
+#     print(i)
+
+# 8393번
+# num = int(input())
+#
+# for i in range(num):
+#     num += i
+#
+# print(num)
+
+# 15552번
+# import sys
+# T = int(sys.stdin.readline())
+# for i in range(T):
+#     a, b = map(int, sys.stdin.readline().split())
+#     print(a + b)
+
+# 2741번
+# num = int(input())
+# for i in range(num):
+#     print(i+1)
+
+# 2742번
+# num = int(input())
+# for i in range(num, 0, -1):
+#     print(i)
