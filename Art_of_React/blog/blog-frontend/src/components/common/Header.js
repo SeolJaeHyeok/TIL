5a60840409fd7dd2a4b94801639f0be6482b6447
Jaehyeok <milkboy2564@naver.com>
2021-06-28 23:08:51 +0900

    로그아웃 기능 구현

--- Art_of_React/blog/blog-frontend/src/components/common/Header.js
+++ Art_of_React/blog/blog-frontend/src/components/common/Header.js
@@ -43,7 +43,7 @@ const UserInfo = styled.div`
   margin-right: 1rem;
 `;
 
-const Header = ({ user }) => {
+const Header = ({ user, onLogout }) => {
   return (
     <>
       <HeaderBlock>
@@ -54,7 +54,7 @@ const Header = ({ user }) => {
           {user ? (
             <div className="right">
               <UserInfo>{user.username}</UserInfo>
-              <Button>로그아웃</Button>
+              <Button onClick={onLogout}>로그아웃</Button>
             </div>
           ) : (
             <div className="right">
