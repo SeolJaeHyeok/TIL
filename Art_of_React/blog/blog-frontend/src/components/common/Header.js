f07cd548d56997fb771fdb3a4665f611471f2da9
Jaehyeok <milkboy2564@naver.com>
2021-06-28 21:30:10 +0900

    Header 컴포넌트 만들어서 적용

---
+++ Art_of_React/blog/blog-frontend/src/components/common/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from './Button';
+import Responsive from './Responsive';
+
+const HeaderBlock = styled.div`
+  position: fixed;
+  width: 100%;
+  background: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+
+/**
+Responsive 컴포넌트의 속성에 스타일을 추가해서 새로운 컴포넌트 생성
+ */
+
+const Wrapper = styled(Responsive)`
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; // 자식 엘리먼트 사이의 간격을 최대로 설정
+  .logo {
+    font-size: 1.125rem;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+/**
+  헤더가 fixed로 되어 있기 때문에 페이지의 콘텐츠가 4rem 아래에 나타나도록 해 주는 컴포넌트
+ */
+const Spacer = styled.div`
+  height: 4rem;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <Link to="/" className="logo">
+            MILKBOY
+          </Link>
+          <div className="right">
+            <Button to="/login">로그인</Button>
+          </div>
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
+};
+
+export default Header;
