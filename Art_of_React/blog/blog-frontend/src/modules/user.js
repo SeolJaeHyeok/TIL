0bf663fcdafa2f886270cd642e7d80c328ae6f6c
Jaehyeok <milkboy2564@naver.com>
2021-06-28 22:51:47 +0900

    로그인 검증 실패 시 정보 초기화

--- Art_of_React/blog/blog-frontend/src/modules/user.js
+++ Art_of_React/blog/blog-frontend/src/modules/user.js
@@ -15,8 +15,18 @@ export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+function checkFailureSaga() {
+  try {
+    localStorage.removeItem('user'); // localStorage에서 user 제거
+  } catch (e) {
+    console.log('LocalStorage is not working');
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 }
 
 const initialState = {
