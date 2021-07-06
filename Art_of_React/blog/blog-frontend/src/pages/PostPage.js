705518cb36e9e3ff9af411a7b672ca6b8073c179
Jaehyeok <milkboy2564@naver.com>
2021-07-06 10:53:26 +0900

    PostViewer UI 준비

--- Art_of_React/blog/blog-frontend/src/pages/PostPage.js
+++ Art_of_React/blog/blog-frontend/src/pages/PostPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PostViewer from '../components/post/PostViewer';
+import HeaderContainer from '../containers/common/HeaderContainer';
 
 const Postpage = () => {
-  return <div>포스트 읽기</div>;
+  return (
+    <>
+      <HeaderContainer />
+      <PostViewer />
+    </>
+  );
 };
 
 export default Postpage;
