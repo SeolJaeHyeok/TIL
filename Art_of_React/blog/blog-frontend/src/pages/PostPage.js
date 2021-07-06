8a5eeb93c64cd8970aad2574e21c1a836c908543
Jaehyeok <milkboy2564@naver.com>
2021-07-06 11:39:30 +0900

    PostViewerContainer 만들어서 리덕스와 연동
    서버에서 데이터 받아와서 화면에 출력

--- Art_of_React/blog/blog-frontend/src/pages/PostPage.js
+++ Art_of_React/blog/blog-frontend/src/pages/PostPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PostViewer from '../components/post/PostViewer';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostViewerContainer from '../containers/post/PostViewerContainer';
 
 const Postpage = () => {
   return (
     <>
       <HeaderContainer />
-      <PostViewer />
+      <PostViewerContainer />
     </>
   );
 };
