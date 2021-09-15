185a472403229c714648f534d9553be0c8ae9b93
Jaehyeok <milkboy2564@naver.com>
2021-09-15 12:34:26 +0900

    Algorithm - Binary Search
    1) 부품 찾기

---
+++ Algorithm/withPython/1st/Chapter 7/7-5.py
@@ -0,0 +1,103 @@
+# 부품 찾기
+# my, 이진 탐색(재귀 함수)이용, solve
+def binary_search(array, target, start, end):
+    if start > end:
+        result.append('no')
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        result.append('yes')
+    elif array[mid] > target:  # 찾으려는 데이터가 중간 값보다 작으면 end를 중간값 바로 왼쪽 값으로 설정 후 재귀
+        return binary_search(array, target, start, mid - 1)
+    else:  # 찾으려는 데이터가 중간 값보다 크면 start를 중간값 바로 오른쪽 값으로 설정 후 재귀
+        return binary_search(array, target, mid + 1, end)
+
+
+n = int(input())
+my = list(map(int, input().split()))
+my.sort()
+m = int(input())
+you = list(map(int, input().split()))
+result = []
+
+for i in range(len(you)):
+    binary_search(my, you[i], 0, n-1)
+
+print(result)
+
+
+# sol1,이진 탐색(반복문) 이용
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        elif array[mid] > target:
+            end = mid - 1
+        # 중간점의 값보다 찾고자 하는 값이 작은 경우 오른쪽 확인
+        else:
+            start = mid + 1
+    return None
+
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부품 번호를 공백을 기준으로 구분하여 입력
+array = list(map(int, input().split()))
+array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M(손님이 확인 요청한 부품 개수) 입력
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백을 기준으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    result = binary_search(array, i, 0, n - 1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+
+# sol2, 계수 정렬 이용
+# N(가게의 부품 개수) 입력
+n = int(input())
+array = [0] * 1000001
+
+# 가게에 있는 전체 부품 번호를 입력 받아서 기록
+for i in input().split():
+    array[int(i)] = 1
+
+# M(손님이 확인 요청한 부품 개수) 입력
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백을 기준으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+# sol3, set() 함수 이용
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부품 번호를 입력 받아서 집합(Set) 자료형에 기록
+array = set(map(int, input().split()))
+
+# M(손님이 확인 요청한 부품 개수) 입력
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백을 기준으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
