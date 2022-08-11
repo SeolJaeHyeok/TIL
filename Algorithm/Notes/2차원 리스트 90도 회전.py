e1c11465d4037c95d302d3df01af3674cc14c4e6
Jaehyeok <milkboy2564@naver.com>
2022-08-11 17:02:59 +0900

    Algorithm - Notes

---
+++ Algorithm/Notes/2차원 리스트 90도 회전.py
@@ -0,0 +1,10 @@
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a) # 행 길이 계산
+    m = len(a[0]) # 열 길이 계산
+    result = [[0] * n for _ in range(m)]
+
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+
+    return result
